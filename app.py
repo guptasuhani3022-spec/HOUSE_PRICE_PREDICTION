493a2eb4ad34eeb6e5df113f22c26c1db6ed528d
guptasuhani3022-spec <guptasuhani3022@gmail.com>
2026-07-08 20:44:14 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import streamlit as st 
-
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-
-st.set_page_config(page_title="Loan Approval App",page__icon="")
